f92116ed99dedeb3d6f67edd33a75b52ca267084
KgaogeloMosetlha <110938292+KgaogeloMosetlha@users.noreply.github.com>
2022-09-15 02:08:25 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kpop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BTS()
+        {
+            return View();
+        }
+
+        public ActionResult SVT()
+        {
+            return View();
+        }
+
+        public ActionResult TXT()
+        {
+            return View();
+        }
+    }
+}
